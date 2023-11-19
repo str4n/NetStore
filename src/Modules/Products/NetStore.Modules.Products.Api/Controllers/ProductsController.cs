66cefb304383a13837244acb4644d3caa9d1261f
Kamil <Str4ngerGameOne@gmail.com>
2023-11-19 20:50:44 +0100

    feat: Products controller.

---
+++ src/Modules/Products/NetStore.Modules.Products.Api/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetStore.Modules.Products.Core.DTO;
+using NetStore.Modules.Products.Core.Services;
+using NetStore.Shared.Infrastructure.Api;
+
+namespace NetStore.Modules.Products.Api.Controllers;
+
+[Route(ProductsModule.BasePath + "/[controller]")]
+internal sealed class ProductsController : BaseController
+{
+    private readonly IProductsService _productsService;
+
+    public ProductsController(IProductsService productsService)
+    {
+        _productsService = productsService;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ProductDto>> Get([FromRoute] Guid id)
+        => OkOrNotFound(await _productsService.GetAsync(id));
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+        => Ok(await _productsService.GetAllAsync());
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] ProductDto dto)
+    {
+        dto = dto with { Id = Guid.NewGuid() };
+        await _productsService.AddAsync(dto);
+
+        return CreatedAtAction(nameof(Get), new { id = dto.Id }, default);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete([FromRoute] Guid id)
+    {
+        await _productsService.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
