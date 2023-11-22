5e845bcf884f10105f1419b7e692bdb0cba87eb8
Kamil <Str4ngerGameOne@gmail.com>
2023-11-22 14:09:24 +0100

    feat(Users module): Password manager.

--- src/Modules/Users/NetStore.Modules.Users.Core/Extensions.cs
+++ src/Modules/Users/NetStore.Modules.Users.Core/Extensions.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetStore.Modules.Users.Core.Domain.Entities;
 using NetStore.Modules.Users.Core.EF;
-using NetStore.Shared.Infrastructure.Postgres;
+using NetStore.Modules.Users.Core.Services;
 
 namespace NetStore.Modules.Users.Core;
 
@@ -10,6 +12,9 @@ internal static class Extensions
     public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddEF(configuration);
+        services
+            .AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>()
+            .AddSingleton<IPasswordManager, PasswordManager>();
         
         return services;
     }
---
+++ src/Modules/Users/NetStore.Modules.Users.Core/Services/IPasswordManager.cs
@@ -0,0 +1,7 @@
+﻿namespace NetStore.Modules.Users.Core.Services;
+
+internal interface IPasswordManager
+{
+    public string Secure(string password);
+    public bool Validate(string password, string securedPassword);
+}
