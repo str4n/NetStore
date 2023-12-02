cd5c3d633dcbbeedd7f06590a67e66ae0b2793ad
Kamil <Str4ngerGameOne@gmail.com>
2023-12-02 15:00:07 +0100

    feat(Catalogs module): Create brand command and handler.

---
+++ src/Modules/Catalogs/NetStore.Modules.Catalogs.Application/Commands/CreateBrand.cs
@@ -0,0 +1,5 @@
+﻿using NetStore.Shared.Abstractions.Commands;
+
+namespace NetStore.Modules.Catalogs.Application.Commands;
+
+public sealed record CreateBrand(string Name) : ICommand;
---
+++ src/Modules/Catalogs/NetStore.Modules.Catalogs.Application/Commands/Handlers/CreateBrandHandler.cs
@@ -0,0 +1,22 @@
+﻿using NetStore.Modules.Catalogs.Domain.Brand;
+using NetStore.Modules.Catalogs.Domain.Repositories;
+using NetStore.Shared.Abstractions.Commands;
+
+namespace NetStore.Modules.Catalogs.Application.Commands.Handlers;
+
+internal sealed class CreateBrandHandler : ICommandHandler<CreateBrand>
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public CreateBrandHandler(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+    
+    public async Task HandleAsync(CreateBrand command)
+    {
+        var brand = Brand.Create(command.Name);
+
+        await _brandRepository.AddAsync(brand);
+    }
+}
