0ec3b8d855647db2d498b9293b9b04b2d6b53a0c
Kamil <Str4ngerGameOne@gmail.com>
2023-12-07 17:07:14 +0100

    fix(Users module): State column renamed.

---
+++ src/Modules/Users/NetStore.Modules.Users.Core/EF/Migrations/20231207160357_Users_Module_StateColumnRename.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetStore.Modules.Users.Core.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users_Module_StateColumnRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserState",
+                schema: "users",
+                table: "Users",
+                newName: "State");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                schema: "users",
+                table: "Users",
+                newName: "UserState");
+        }
+    }
+}
